d74d99299453b333925472e0257791a1e6316933
kevin <kalnikkevin@gmail.com>
2025-09-15 21:20:09 +0300

    first commit

---
+++ types/note.ts
@@ -0,0 +1,15 @@
+export interface Note {
+    id: string,
+    title: string,
+    content: string,
+    tag: string,
+    createdAt: string,
+    updatedAt: string,
+}
+
+
+export interface NoteInputValues {
+    title: string,
+    content?: string,
+    tag: string,
+}
